374081916e838e63be7eef67998b4b28613fb747
ritam34 <ritamcoder@gmail.com>
2025-12-04 20:52:03 +0530

    feat: remove execute code controller, error middleware, routes, and contest validators

--- backend/src/routes/executeCode.routes.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import { executeCode } from '../controllers/executeCode.controllers.js';
-
-const router = Router();
-
-router.route("/")
-    .post(executeCode)
-
-export default router;
